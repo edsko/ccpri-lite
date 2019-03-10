9cce79807c8321a998c4b077b6342da2df200356
Edsko de Vries <edsko@well-typed.com>
2019-03-10 17:59:45 +0100

    Comment

--- js/receipt.js
+++ js/receipt.js
@@ -2,6 +2,9 @@ function init() {
   // Get receipt contents from parent
   var receipt = window.opener.receipt;
   document.getElementById("receipt").innerHTML = "receipt: " + receipt;
+
+  // Print and close
+  // To avoid any dialogues, can enable Chrome's --kiosk-printing mode
   window.print();
   window.close();
 }
