a00e1b70a48f72a44eeedfd1c2cfdec788b49ccb
Edsko de Vries <edsko@well-typed.com>
2019-03-10 10:10:42 +0100

    Basic selection working

--- js/ccpri.js
+++ js/ccpri.js
@@ -4,6 +4,37 @@
 
 var version = "0.1";
 
+/*
+ * Setup
+ */
+
+/* Types of the fields on the main screen */
+var types = {
+    "type":        "type"
+  , "nationality": "nationality"
+  , "nights":      "int"
+  , "adults":      "int"
+  , "children":    "int"
+  , "electricity": "bool"
+  }
+
+/*
+ * Global variables
+ */
+
+/* Currently selected values */
+var selected = {
+    "type":        "5-caravan"
+  , "nationality": "01-IE"
+  , "nights":      "000"
+  , "adults":      "000"
+  , "children":    "000"
+  , "electricity": "false"
+  }
+
+/*
+ * Initialize
+ */
 function init() {
   console.log("CCPRI Lite " + version);
 
@@ -12,4 +43,37 @@ function init() {
   } else {
     console.log("ERROR: Web Storage unsupported");
   }
+
+  markSelected();
+}
+
+/*
+ * Mark selected elements based on the current value of `selected`
+ */
+function markSelected() {
+  for (var field in selected) {
+    let value = selected[field];
+    let type  = types[field];
+    let elem  = document.getElementById(field + "-" + value);
+    elem.src = "img/buttons/selected/" + type + "/" + value + ".jpg";
+  }
+}
+
+/*
+ * Deselect old value
+ */
+function unmarkSelectedField(field) {
+  var value = selected[field];
+  var type  = types[field];
+  var elem  = document.getElementById(field + "-" + value);
+  elem.src = "img/buttons/unselected/" + type + "/" + value + ".jpg";
+}
+
+/*
+ * Update `selected` in response to user selection
+ */
+function select(field, value) {
+  unmarkSelectedField(field);
+  selected[field] = value;
+  markSelected();
 }
